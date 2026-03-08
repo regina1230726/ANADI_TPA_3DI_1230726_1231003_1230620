9bc5d712dc35c7a0221c72a7e9fb168b525443eb
reginasilva <1230726@isep.ipp.pt>
2026-03-08 14:40:42 +0000

    4.3.3 - quantificação de valores omissos

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # 4.2 MANIPULAÇÃO DE DADOS
 
-# 1. Ficheiro IP
+# 4.2.1. Ficheiro IP
 
-# 2. Ficheiro PTD
+# 4.2.2. Ficheiro PTD
 
 def converter_utilizacao(valor):
 
@@ -36,6 +37,7 @@ ptd = pd.read_excel("PTD_data.xlsx")
 
 ptd["Utilizacao_decimal"] = ptd["Nível de Utilização [%]"].apply(converter_utilizacao)
 
+print("4.2.2: Conversão de nível de utilização para decimal:")
 print(ptd[["Nível de Utilização [%]", "Utilizacao_decimal"]].head())
 
 ptd_group = ptd.groupby("CodDistritoConcelho").agg(
@@ -48,5 +50,35 @@ ptd_group = ptd.groupby("CodDistritoConcelho").agg(
 
 ).reset_index()
 
+print()
+print("4.2.2: Agrupar por Código Distrito Concelho")
 print(ptd_group.head())
 
+# 4.3 ANÁLISE E EXPLORAÇÃO DE DADOS
+
+# 4.3.3 - Quantificar valores omissos ou indeterminados
+
+nd_count = (ptd["Nível de Utilização [%]"] == "N/D").sum()
+lt20_count = ptd["Nível de Utilização [%]"].astype(str).str.contains("<20").sum()
+
+total = len(ptd)
+
+print()
+print("4.3.3: Quantidade de valores omissos ou indeterminados:")
+print("Valores N/D:", nd_count)
+print("Valores <20:", lt20_count)
+print("Total de registos:", total)
+
+print("Percentagem N/D:", round((nd_count/total)*100,2), "%")
+print("Percentagem <20:", round((lt20_count/total)*100,2), "%")
+
+# remover NaN para análise
+utilizacao = ptd["Utilizacao_decimal"].dropna()
+
+plt.figure(figsize=(8,5))
+plt.boxplot(utilizacao)
+
+plt.title("Distribuição do Nível de Utilização da Rede")
+plt.ylabel("Utilização da Rede")
+
+plt.show()
